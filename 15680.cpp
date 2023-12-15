c25b6e67635b99d67282a8b65f3a2f1323dabc33
Jeong ji won <imprayer1@naver.com>
2023-12-15 13:43:37 +0900

    연세대학교

---
+++ 15680.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int a=0;
+    std::cin >> a;
+    if(a==0)
+    {
+        std::cout<<"YONSEI"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Leading the Way to the Future"<<std::endl;
+    }
+}
