5fde46c627d78afb54510081ab0999bd903b7461
Jeong ji won <imprayer1@naver.com>
2023-12-19 23:28:08 +0900

    이 별은 무슨 색일까

---
+++ 23825.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    int a, b;
+    std::cin >> a >> b;
+    if((a/2) > (b/2))
+    {
+        std::cout << b/2;
+    }
+    else if((a/2) < (b/2))
+    {
+        std::cout << a/2;
+    }
+    else
+    {
+        std::cout << a/2;
+    }
+}
